9ba806e860418e3fd8099e2791e57c1645364b79
Ozgun Babur <ozgunbabur@gmail.com>
2022-11-13 17:21:34 -0500

    Added more trajectory analysis.

--- scripts/check_coords.py
+++ scripts/check_coords.py
@@ -1,7 +1,21 @@
+import numpy as np
 import pandas as pd
 
-def check_coords(cell_array,umap_coords_path):
-    umap_coords = pd.read_table(umap_coords_path,index_col=0)
+
+def check_coords(cell_array, umap_coords_path):
+    umap_coords = pd.read_table(umap_coords_path, index_col=0)
     traject = umap_coords[umap_coords.index.isin(cell_array)]
     ord_traject = traject.loc[cell_array]
     return(ord_traject)
+
+
+def get_line_map(trajectory, umap_coords_path):
+    umap_coords = pd.read_table(umap_coords_path, index_col=0)
+    lines = np.zeros((2, len(trajectory)))
+    i = 0
+    for t in trajectory:
+        lines[0, i] = umap_coords.loc[t][0]
+        lines[1, i] = umap_coords.loc[t][1]
+        i = i+1
+
+    return lines
