875d4c009036c7af5f52600d79bd5eef01fe0825
KisanThapa <kisanthapa33@gmail.com>
2023-01-04 14:57:18 -0500

    Update analysis parameters

--- analysis/urls.py
+++ analysis/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('get_all/', views.get_all_analysis, name='get_all_analysis'),
     path('run/', views.run_analysis, name='run_analysis'),
+    path('update/', views.update_analysis, name='update_analysis'),
     path('delete/', views.delete_analysis, name='delete_analysis'),
     path('get_coordinates/', views.get_coordinates, name='get_coordinates')
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -5,7 +5,6 @@ import shutil
 from django.http import JsonResponse, HttpResponse
 
 from file_manage.views import httpMethodError, convert_date, safe_open_w
-from pipeline.views import runPipeline
 
 
 def get_all_analysis(request):
@@ -69,42 +68,99 @@ def delete_analysis(request):
         return httpMethodError("POST", request.method)
 
 
+def create_param_file(req_body):
+    param_file = {
+        "analysisName": req_body['analysisName'],
+        "dataMatrixFile": req_body['dataMatrixFile'],
+        "metaDataFile": req_body['metaDataFile'],
+        "useFiltering": req_body['useFiltering'],
+        "usePCA": req_body['usePCA'],
+        "useUMAP": req_body['useUMAP'],
+        "normalizationScale": req_body['normalizationScale'],
+    }
+
+    if req_body['useFiltering']:
+        param_file['minNumOfCells'] = req_body['minNumOfCells']
+        param_file['minNumOfGenes'] = req_body['minNumOfGenes']
+        param_file['qcFilterPerc'] = req_body['qcFilterPerc']
+
+    if req_body['usePCA']:
+        param_file['pcaCount'] = req_body['pcaCount']
+    else:
+        param_file['pcaFile'] = req_body['pcaFile']
+
+    if req_body['useUMAP']:
+        param_file['n_neighbors'] = req_body['n_neighbors']
+        param_file['min_dist'] = req_body['min_dist']
+        param_file['metric'] = req_body['metric']
+    else:
+        param_file['umapFile'] = req_body['umapFile']
+
+    return param_file
+
+
 def run_analysis(request):
     if request.method == "POST":
         try:
             req_body = json.loads(request.body.decode('utf-8'))
             print('req_body', req_body)
 
-            param_file = {
-                "analysisName": req_body['analysisName'],
-                "dataMatrixFile": req_body['dataMatrixFile'],
-                "metaDataFile": req_body['metaDataFile'],
-            }
+            # Create params.json file content
+            new_param_file = create_param_file(req_body)
 
-            if req_body['usePCA']:
-                param_file['usePCA'] = req_body['usePCA']
-                param_file['pcaCount'] = req_body['pcaCount']
-            else:
-                param_file['usePCA'] = req_body['usePCA']
-                param_file['pcaFile'] = req_body['pcaFile']
+            # Convert into json
+            param_file_json = json.dumps(new_param_file, indent=4)
 
-            if req_body['useUMAP']:
-                param_file['useUMAP'] = req_body['useUMAP']
-                param_file['n_neighbors'] = req_body['n_neighbors']
-                param_file['min_dist'] = req_body['min_dist']
-                param_file['metric'] = req_body['metric']
-            else:
-                param_file['useUMAP'] = req_body['useUMAP']
-                param_file['umapFile'] = req_body['umapFile']
-
-            param_file_json = json.dumps(param_file, indent=4)
             analysis_file = 'data/kisan@gmail.com/analysis/' + req_body['analysisName'] + '/params.json'
             with safe_open_w(analysis_file) as f:
                 f.write(param_file_json)
 
-
             # run pipeline
-            runPipeline(request)
+            # runPipeline(request)
+
+            return HttpResponse(status=200)
+
+        except Exception as e:
+            print(e)
+            return HttpResponse('Exception: ' + str(e), status=404)
+
+    else:
+        print(request)
+        return httpMethodError("GET", request.method)
+
+
+def update_analysis(request):
+    if request.method == "POST":
+        try:
+            req_body = json.loads(request.body.decode('utf-8'))
+            print('req_body: ', req_body)
+
+            analysis_name = req_body['analysisName']
+            old_analysis_name = req_body['oldAnalysisName']
+
+            # Create params.json file content
+            new_param_file = create_param_file(req_body)
+
+            # Convert into json
+            param_file_json = json.dumps(new_param_file, indent=4)
+
+            # Check if analysis name is changed, if is not changed then update the same file
+            if analysis_name == old_analysis_name:
+                path = 'data/kisan@gmail.com/analysis/' + old_analysis_name + '/params.json'
+                with safe_open_w(path) as f:
+                    f.write(param_file_json)
+            else:
+                # Rename analysis folder
+                old_path = 'data/kisan@gmail.com/analysis/' + old_analysis_name
+                new_path = 'data/kisan@gmail.com/analysis/' + analysis_name
+                os.rename(old_path, new_path)
+
+                new_path = new_path + '/params.json'
+
+                new_param_file['analysisName'] = analysis_name
+                param_file_json = json.dumps(new_param_file, indent=4)
+                with safe_open_w(new_path) as f:
+                    f.write(param_file_json)
 
             return HttpResponse(status=200)
 
