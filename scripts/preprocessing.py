deabc3dc918707eddaa8e954c571c9bd221b27fa
Ozgun Babur <ozgunbabur@gmail.com>
2023-01-06 09:18:20 -0500

    Added type hints.

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -2,8 +2,10 @@ import pandas as pd
 import scanpy as sc
 
 
-def filter_and_normalize(species, apply_min_cells, apply_min_genes, apply_mt_filter, apply_normalization, apply_log,
-                         exp_path, out_path, min_genes=200, min_cells=10, target_sum=1e4, percent_mt=10):
+def filter_and_normalize(species: str, apply_min_cells: bool, apply_min_genes: bool, apply_mt_filter: bool,
+                         apply_normalization: bool, apply_log: bool, exp_path: str, out_path: str,
+                         min_genes: int = 200, min_cells: int = 10, target_sum: int = 1e4,
+                         percent_mt: float = 10) -> None:
     adata = sc.read(exp_path).T
 
     if apply_min_cells:
@@ -17,7 +19,7 @@ def filter_and_normalize(species, apply_min_cells, apply_min_genes, apply_mt_fil
         elif species == "Human":
             adata.var['mt'] = adata.var_names.str.startswith('MT-')
         else:
-            raise Exception("This specie is not supported")
+            raise Exception("This species is not supported: " + species)
 
         sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
         adata = adata[adata.obs.pct_counts_mt < percent_mt, :]
